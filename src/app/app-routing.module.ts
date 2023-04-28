94e4b20ec1e9a9f64fc995cd5ddd7361263b017d
Luis Monterroso <namenotfound4004@gmail.com>
2023-04-28 00:17:10 -0600

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   {
-    path: '',
+    path: 'menu-docente',
     component: MenuDocenteComponent,
     children: [
     ],
