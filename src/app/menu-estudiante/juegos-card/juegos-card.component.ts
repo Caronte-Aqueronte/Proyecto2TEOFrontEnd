9ad4d40cb99fc03124828050f3c9cc91008a316c
karlamatias99 <karlamatias201830032@cunoc.edu.gt>
2023-05-19 15:02:42 -0600

    Cambios Fronted

--- src/app/menu-estudiante/juegos-card/juegos-card.component.ts
+++ src/app/menu-estudiante/juegos-card/juegos-card.component.ts
@@ -9,10 +9,44 @@ import { Router } from '@angular/router';
 export class JuegosCardComponent {
   @Input() juego: any;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   public aJuegoPerfil(): void {
     this.router.navigate([]);
     this.router.navigate([`menu-estudiante/juegos-perfil/${this.juego._id}`]);
   }
+
+  /*getImagenJuego(tipo: string): string {
+    if (tipo === 'Quiz') {
+      return 'url(../../../assets/img/quizz.png)';
+    } else if (tipo === 'Ahorcado') {
+      return 'url(../../../assets/img/juego-del-ahorcado.png)';
+    } else if (tipo === 'Memoria') {
+      return 'url(../../../assets/img/memoria.png)';
+    } else if (tipo === 'Ordena palabra') {
+      return 'url(../../../assets/img/ordenaPalabra.png)';
+    } else {
+      // Tipo de juego desconocido
+      return '';
+    }
+  }*/
+  getImagenJuego(tipoJuego: string): string {
+    let imagen: string;
+
+    // Asigna la ruta de la imagen según el tipo de juego
+    if (tipoJuego === 'Quiz') {
+      imagen = '../../../assets/img/quizz.png';
+    } else if (tipoJuego === 'Ahorcado') {
+      imagen = '../../../assets/img/juego-del-ahorcado.png';
+    } else if (tipoJuego === 'Memoria') {
+      imagen = '../../../assets/img/memoria.png';
+    } else if (tipoJuego === 'Ordena palabra') {
+      imagen = '../../../assets/img/ordenaPalabra.png';
+    } else {
+      imagen = ''; // Ruta de imagen por defecto o en caso de tipo de juego desconocido
+    }
+
+    return imagen;
+  }
+
 }
